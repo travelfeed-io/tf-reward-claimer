1cf76864b68e410628181ad851483a8d48a84931
tiotdev <tiotdev@tiot.de>
2019-10-28 22:45:27 +0800

    Implement claiming rewards balance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const { getTrackedUsers } = require('./db/db');
+const { getPendingRewards, broadcastClaim } = require('./steem/steem');
 
 getTrackedUsers().then(users => {
   users.forEach(user => {
-    // TODO: Claim rewards balance
-    console.log(`Claiming rewards balance for ${user}`);
+    getPendingRewards(user).then(pendingRewards => {
+      const {
+        reward_sbd_balance,
+        reward_steem_balance,
+        reward_vesting_balance,
+      } = pendingRewards;
+      if (
+        reward_sbd_balance !== '0.000 SBD' ||
+        reward_steem_balance !== '0.000 STEEM' ||
+        reward_vesting_balance !== '0.000000 VESTS'
+      ) {
+        broadcastClaim(
+          user,
+          reward_sbd_balance,
+          reward_steem_balance,
+          reward_vesting_balance,
+        );
+      } else console.log(`Nothing to claim for user ${user}`);
+    });
   });
 });
---
+++ src/steem/steem.js
@@ -0,0 +1,43 @@
+const steem = require('steem');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const privatePostingWif = process.env.TF_POSTING_PRIVATE;
+
+const getPendingRewards = account => {
+  return steem.api.getAccountsAsync([account]).then(r => {
+    const {
+      reward_sbd_balance,
+      reward_steem_balance,
+      reward_vesting_balance,
+    } = r[0];
+    return { reward_sbd_balance, reward_steem_balance, reward_vesting_balance };
+  });
+};
+
+const broadcastClaim = (
+  account,
+  reward_sbd_balance,
+  reward_steem_balance,
+  reward_vesting_balance,
+) => {
+  steem.broadcast.claimRewardBalance(
+    privatePostingWif,
+    account,
+    reward_steem_balance,
+    reward_sbd_balance,
+    reward_vesting_balance,
+    function(err, result) {
+      if (err)
+        console.warn(
+          `Could not claim rewards balance for ${account}: ${result}`,
+        );
+      else console.log(`Claimed rewards balance for ${account}: ${result}`);
+    },
+  );
+};
+
+module.exports = {
+  getPendingRewards,
+  broadcastClaim,
+};
